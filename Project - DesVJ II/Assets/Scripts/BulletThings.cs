bf2e3f00c09e59b69b0ff3cd382bfd8e70da749c
Unknown <ale.bros@hotmail.com>
2018-04-18 11:31:00 -0300

    First Push

---
+++ Project - DesVJ II/Assets/Scripts/BulletThings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletThings : MonoBehaviour
+{
+    [SerializeField] float movementSpeed;
+
+    [SerializeField] float lifeTime;
+
+    private void Update ()
+    {
+        transform.Translate(0, 0, movementSpeed * Time.deltaTime);
+
+        Invoke("EndLife", lifeTime);
+	}
+
+    private void EndLife()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Project - DesVJ II/Assets/Scripts/Ejemplo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejemplo : MonoBehaviour
+{
+    [SerializeField] float velocity; // Es privada pero se puede editar en el editor.
+
+    [SerializeField] Color color;
+
+    [SerializeField] Vector3 spawnPoint;
+
+    [SerializeField] string name;
+
+    [SerializeField] bool canJump;
+
+    private void Awake()
+    {
+        print("Awake");
+    }
+
+    private void Update()
+    {
+        print("Update");
+    }
+
+    private void OnDestroy()
+    {
+        print("OnDestroy");
+    }
+
+    private void OnBecameInvisible()
+    {
+        print("OnBecameInvisible");
+    }
+
+    private void OnMouseDown()
+    {
+        print("OnMouseDown");
+    }
+}
