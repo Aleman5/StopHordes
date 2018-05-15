5012607fb1f9dd6ecde76919558ae9157d0f623b
Unknown <ale.bros@hotmail.com>
2018-05-15 00:34:53 -0300

    Some skins added, Bullet implemented and a better UI

--- Project - DesVJ II/Assets/Scripts/ScrGenerals/Health.cs
+++ Project - DesVJ II/Assets/Scripts/ScrGenerals/Health.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using EZCameraShake;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] float mount;
     [SerializeField] Camera camera;
+
+    [SerializeField] UnityEvent onHealthChanged;
+
+    public UnityEvent OnHealthChanged
+    {
+        get { return onHealthChanged; }
+    }
 
     public float Mount
     {
@@ -16,13 +24,19 @@ public class Health : MonoBehaviour
             mount = value;
             if (mount <= 0)
             {
-                if (camera)
-                    camera.transform.SetParent(null);
                 mount = 0;
+                if (camera)
+                {
+                    camera.transform.SetParent(null);
+                    OnHealthChanged.Invoke();
+                }
                 Destroy(gameObject);
             }
             else if (camera)
+            {
                 CameraShaker.Instance.ShakeOnce(4f, 4f, .1f, 1f);
+                OnHealthChanged.Invoke();
+            }
         }
     }
 }
--- Project - DesVJ II/Assets/Scripts/ScrUI/HPUI.cs
+++ Project - DesVJ II/Assets/Scripts/ScrUI/HPUI.cs
@@ -1,8 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HPUI : MonoBehaviour
 {
+    private Image image;
 
+    [SerializeField] Health health;
+
+    private void Start()
+    {
+        health.OnHealthChanged.AddListener(RefreshHealth);
+        RefreshHealth();
+    }
+    private void RefreshHealth()
+    {
+        image.fillAmount = health.Mount;
+    }
 }
